151fb7731b34ff42feb58404376dd4fe42d6b435
joe <draxil@gmail.com>
2016-11-07 19:06:10 +0000

    Make test more graceful when gearman not installed

--- worker/stubborn/stubborn_test.go
+++ worker/stubborn/stubborn_test.go
@@ -1,17 +1,20 @@
 package stubborn
 
 import (
-	"testing"
-	"github.com/mikespook/gearman-go/worker"
+	"errors"
 	"github.com/mikespook/gearman-go/client"
+	"github.com/mikespook/gearman-go/worker"
 	"log"
 	"net"
+	"os"
 	"os/exec"
-	"time"
 	"sync"
-	"errors"
+	"testing"
+	"time"
 )
+
 const port = `3700`
+const gearmand = `/usr/sbin/gearmand`
 
 var gearman_ready chan bool
 var kill_gearman chan bool
@@ -26,7 +29,7 @@ func init() {
 }
 
 func run_gearman() {
-	gm_cmd := exec.Command(`/usr/sbin/gearmand`, `--port`, port)
+	gm_cmd := exec.Command(gearmand, `--port`, port)
 	start_err := gm_cmd.Start()
 
 	if start_err != nil {
@@ -49,8 +52,8 @@ func run_gearman() {
 		panic(`Unable to start gearman aborting test`)
 	}
 	gearman_ready <- true
-	
-	<- kill_gearman
+
+	<-kill_gearman
 }
 
 func check_gearman_present() bool {
@@ -72,22 +75,29 @@ func check_gearman_is_dead() bool {
 	}
 	return false
 }
+func skip_if_no_gearman(t *testing.T) {
+	_, err := os.Stat(gearmand)
+	if err != nil && os.IsNotExist(err) {
+		t.Skip("Skipping no real gearman to test with")
+	}
+}
 func TestBasicDisconnectWithStubbornErrh(t *testing.T) {
+	skip_if_no_gearman(t)
 	go run_gearman()
 	<-gearman_ready
 
 	w := worker.New(worker.Unlimited)
 	timeout := make(chan bool, 1)
 	done := make(chan bool, 1)
-	
-	w.ErrorHandler = MakeErrorHandler( nil )
+
+	w.ErrorHandler = MakeErrorHandler(nil)
 
 	if err := w.AddFunc("stubborn-job",
 		func(j worker.Job) (b []byte, e error) {
-		log.Println("job happens");
+			log.Println("job happens")
 
-		done <- true
-		return
+			done <- true
+			return
 		}, 0); err != nil {
 		t.Error(err)
 	}
@@ -95,12 +105,11 @@ func TestBasicDisconnectWithStubbornErrh(t *testing.T) {
 		t.Error(err)
 	}
 
-
 	go func() {
 		time.Sleep(5 * time.Second)
 		timeout <- true
 	}()
-	
+
 	err := w.Ready()
 
 	if err != nil {
@@ -108,14 +117,14 @@ func TestBasicDisconnectWithStubbornErrh(t *testing.T) {
 	}
 
 	go w.Work()
-	if ! check_gearman_present(){
+	if !check_gearman_present() {
 		t.Error("Sanity: no gearman server?")
 	}
 	// First check worker is in place:
 	send_client_request()
 
 	select {
-	case <- done:
+	case <-done:
 	case <-timeout:
 		t.Error("Test timed out: initial job")
 	}
@@ -126,25 +135,25 @@ func TestBasicDisconnectWithStubbornErrh(t *testing.T) {
 	//
 	go run_gearman()
 	<-gearman_ready
-	if ! check_gearman_present(){
+	if !check_gearman_present() {
 		t.Error("Sanity: no gearman server?")
 	}
 
 	send_client_request()
 
 	select {
-	case <- done:
+	case <-done:
 	case <-timeout:
 		t.Error("Test timed out: reconnect job")
 	}
-
 
 	w.Close()
 	kill_gearman <- true
 }
 
 func TestBasicDisconnectWithStubbornWorker(t *testing.T) {
-	check_gearman_is_dead();
+	skip_if_no_gearman(t)
+	check_gearman_is_dead()
 	go run_gearman()
 	<-gearman_ready
 
@@ -154,10 +163,10 @@ func TestBasicDisconnectWithStubbornWorker(t *testing.T) {
 
 	if err := w.AddFunc("stubborn-job",
 		func(j worker.Job) (b []byte, e error) {
-		log.Println("job happens");
+			log.Println("job happens")
 
-		done <- true
-		return
+			done <- true
+			return
 		}, 0); err != nil {
 		t.Error(err)
 	}
@@ -165,12 +174,11 @@ func TestBasicDisconnectWithStubbornWorker(t *testing.T) {
 		t.Error(err)
 	}
 
-
 	go func() {
 		time.Sleep(5 * time.Second)
 		timeout <- true
 	}()
-	
+
 	err := w.Ready()
 
 	if err != nil {
@@ -178,14 +186,14 @@ func TestBasicDisconnectWithStubbornWorker(t *testing.T) {
 	}
 
 	go w.Work()
-	if ! check_gearman_present(){
+	if !check_gearman_present() {
 		t.Error("Sanity: no gearman server?")
 	}
 	// First check worker is in place:
 	send_client_request()
 
 	select {
-	case <- done:
+	case <-done:
 	case <-timeout:
 		t.Error("Test timed out: initial job")
 	}
@@ -196,53 +204,52 @@ func TestBasicDisconnectWithStubbornWorker(t *testing.T) {
 	//
 	go run_gearman()
 	<-gearman_ready
-	if ! check_gearman_present(){
+	if !check_gearman_present() {
 		t.Error("Sanity: no gearman server?")
 	}
 
 	send_client_request()
 
 	select {
-	case <- done:
+	case <-done:
 	case <-timeout:
 		t.Error("Test timed out: reconnect job")
 	}
-
 
 	w.Close()
 	kill_gearman <- true
 }
 
 func TestBasicDisconnectWithShouldReconnectHandler(t *testing.T) {
-	check_gearman_is_dead();
+	skip_if_no_gearman(t)
+	check_gearman_is_dead()
 	go run_gearman()
 	<-gearman_ready
-	
+
 	timeout := make(chan bool, 1)
 	done := make(chan bool, 1)
-	
+
 	should_reconnect := true
 	var should_reconnect_mutex sync.Mutex
-	srh := func( *worker.WorkerDisconnectError )(bool){ 
-		defer func(){ 
+	srh := func(*worker.WorkerDisconnectError) bool {
+		defer func() {
 			should_reconnect_mutex.Unlock()
 		}()
 		should_reconnect_mutex.Lock()
 		return should_reconnect
 	}
-	
 
-	w := NewStubbornWorker(worker.Unlimited, &Settings{ 
-		ShouldReconnectHandler : srh,
-	        ReconnectDelay : 10 * time.Millisecond, // make this SUPER quick for this one
+	w := NewStubbornWorker(worker.Unlimited, &Settings{
+		ShouldReconnectHandler: srh,
+		ReconnectDelay:         10 * time.Millisecond, // make this SUPER quick for this one
 	})
-	
+
 	if err := w.AddFunc("stubborn-job",
 		func(j worker.Job) (b []byte, e error) {
-		log.Println("job happens");
+			log.Println("job happens")
 
-		done <- true
-		return
+			done <- true
+			return
 		}, 0); err != nil {
 		t.Error(err)
 	}
@@ -250,12 +257,11 @@ func TestBasicDisconnectWithShouldReconnectHandler(t *testing.T) {
 		t.Error(err)
 	}
 
-
 	go func() {
 		time.Sleep(5 * time.Second)
 		timeout <- true
 	}()
-	
+
 	err := w.Ready()
 
 	if err != nil {
@@ -263,14 +269,14 @@ func TestBasicDisconnectWithShouldReconnectHandler(t *testing.T) {
 	}
 
 	go w.Work()
-	if ! check_gearman_present(){
+	if !check_gearman_present() {
 		t.Error("Sanity: no gearman server?")
 	}
 	// First check worker is in place:
 	send_client_request()
 
 	select {
-	case <- done:
+	case <-done:
 	case <-timeout:
 		t.Error("Test timed out: initial job")
 	}
@@ -281,25 +287,25 @@ func TestBasicDisconnectWithShouldReconnectHandler(t *testing.T) {
 	//
 	go run_gearman()
 	<-gearman_ready
-	if ! check_gearman_present(){
+	if !check_gearman_present() {
 		t.Error("Sanity: no gearman server?")
 	}
 
 	send_client_request()
 
 	select {
-	case <- done:
+	case <-done:
 	case <-timeout:
 		t.Error("Test timed out: reconnect job")
 	}
 
-	quick_timeout_test := func () (bool){
+	quick_timeout_test := func() bool {
 		kill_gearman <- true
 		check_gearman_is_dead()
 		//
 		go run_gearman()
 		<-gearman_ready
-		if ! check_gearman_present(){
+		if !check_gearman_present() {
 			t.Error("Sanity: no gearman server?")
 		}
 
@@ -310,10 +316,8 @@ func TestBasicDisconnectWithShouldReconnectHandler(t *testing.T) {
 			quick_timeout <- true
 		}()
 
-
-
 		select {
-		case <- done:
+		case <-done:
 			return false
 		case <-quick_timeout:
 			return true
@@ -321,7 +325,7 @@ func TestBasicDisconnectWithShouldReconnectHandler(t *testing.T) {
 
 	}
 
-	if  quick_timeout_test() {
+	if quick_timeout_test() {
 		t.Error("Control test for cancelling reconnect failed. Dubious! Or the test isn't locked down well enough")
 	}
 
@@ -331,57 +335,56 @@ func TestBasicDisconnectWithShouldReconnectHandler(t *testing.T) {
 	should_reconnect = false
 	should_reconnect_mutex.Unlock()
 
-	if ! quick_timeout_test(){
-	 	t.Error("Seemingly the ShouldReconnectHandler didn't stop us reconnecting!")
+	if !quick_timeout_test() {
+		t.Error("Seemingly the ShouldReconnectHandler didn't stop us reconnecting!")
 	}
-
 
 	w.Close()
 	kill_gearman <- true
 }
 func TestBasicDisconnectWithSubErrHandler(t *testing.T) {
-	check_gearman_is_dead();
+	skip_if_no_gearman(t)
+	check_gearman_is_dead()
 	go run_gearman()
 	<-gearman_ready
-	
+
 	timeout := make(chan bool, 1)
 	done := make(chan bool, 1)
 	errdone := make(chan bool, 1)
-	
+
 	handled := false
 	var handled_mutex sync.Mutex
-	erh := func( e error){ 
+	erh := func(e error) {
 		handled_mutex.Lock()
 		handled = true
 		handled_mutex.Unlock()
-		log.Println( e )
+		log.Println(e)
 		errdone <- true
 	}
-	was_handled := func()(rv bool){
+	was_handled := func() (rv bool) {
 		handled_mutex.Lock()
 		rv = handled
 		handled_mutex.Unlock()
 		return
 	}
-	
 
-	w := NewStubbornWorker(worker.Unlimited, &Settings{ 
-		ShouldReconnectHandler : func(*worker.WorkerDisconnectError)(bool){ log.Println("SHOULD RC?");return false },
-	        ErrorHandler : erh,
+	w := NewStubbornWorker(worker.Unlimited, &Settings{
+		ShouldReconnectHandler: func(*worker.WorkerDisconnectError) bool { log.Println("SHOULD RC?"); return false },
+		ErrorHandler:           erh,
 	})
 	job := 0
 	if err := w.AddFunc("stubborn-job",
 		func(j worker.Job) (b []byte, e error) {
-		log.Println("job happens");
-		
-		if job == 1 {
-			e = errors.New("woo")
-			return 
-		}
-		job++
+			log.Println("job happens")
 
-		done <- true
-		return
+			if job == 1 {
+				e = errors.New("woo")
+				return
+			}
+			job++
+
+			done <- true
+			return
 		}, 0); err != nil {
 		t.Error(err)
 	}
@@ -389,12 +392,11 @@ func TestBasicDisconnectWithSubErrHandler(t *testing.T) {
 		t.Error(err)
 	}
 
-
 	go func() {
 		time.Sleep(5 * time.Second)
 		timeout <- true
 	}()
-	
+
 	err := w.Ready()
 
 	if err != nil {
@@ -402,33 +404,32 @@ func TestBasicDisconnectWithSubErrHandler(t *testing.T) {
 	}
 
 	go w.Work()
-	if ! check_gearman_present(){
+	if !check_gearman_present() {
 		t.Error("Sanity: no gearman server?")
 	}
 	// First check worker is in place:
 	send_client_request()
 
 	select {
-	case <- done:
+	case <-done:
 	case <-timeout:
 		t.Error("Test timed out: initial job")
-	case <- errdone:
+	case <-errdone:
 		t.Error("Unexpected error?")
 	}
 
-	if  was_handled() {
+	if was_handled() {
 		t.Error("unexpected fire of error handler")
 	}
 	send_client_request()
 
-
 	select {
-	case <- errdone:
+	case <-errdone:
 	case <-timeout:
 		t.Error("Test timed out: err")
 	}
 
-	if ! was_handled() {
+	if !was_handled() {
 		t.Error("Seemingly errh did not fire?")
 	}
 
